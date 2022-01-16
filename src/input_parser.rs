49ee8bf71bc38eecd1cdb56bc89e1746b91e1a92
Erik Schroeder <schroedererik@mailbox.org>
2022-01-16 19:31:18 +0100

    Remove unused state variable

--- src/input_parser.rs
+++ src/input_parser.rs
@@ -3,13 +3,11 @@ use anyhow::Result;
 use std::{fs::File, io::Read};
 
 #[derive(Debug)]
-pub struct InputParser {
-    pub state: u32,
-}
+pub struct InputParser {}
 
 impl InputParser {
     pub fn new() -> Result<InputParser> {
-        Ok(InputParser { state: 0 })
+        Ok(InputParser {})
     }
 
     pub fn parse_transactions(self, file: &str) -> Result<Vec<Transaction>> {
