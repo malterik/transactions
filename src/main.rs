bb09c92ffbc6ba52c13f1ef804543a2d57119a48
Erik Schroeder <schroedererik@mailbox.org>
2022-01-23 13:19:27 +0100

    Remove debug prints

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ mod transaction;
 mod transaction_engine;
 
 use input_parser::InputParser;
-use tokio::time::Instant;
 use transaction_engine::TransactionEngine;
 
 use std::env;
@@ -15,14 +14,9 @@ async fn main() {
         println!("Please enter a csv file with transactions");
         return;
     }
-    let mut now = Instant::now();
     let parser = InputParser::new().unwrap();
     let transactions = parser.parse_transactions(&args[1]).await.unwrap();
-    println!("File parsed in: {} ms", now.elapsed().as_millis());
-    now = Instant::now();
-
     let mut engine = TransactionEngine::new().unwrap();
     engine.process(&transactions);
-    println!("Transactions processed in: {} ms", now.elapsed().as_millis());
     engine.print_client_list();
 }
